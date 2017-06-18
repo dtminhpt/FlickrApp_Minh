b455efb6cd5c9fca3e10d6899176bf9ad94a97d5
Minh Dinh <dtminhpt@gmail.com>
2017-06-18 07:30:26 +0700

    Remove redundant code

--- index.ios.js
+++ index.ios.js
@@ -31,20 +31,4 @@ export default class FlickrApp extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('FlickrApp', () => FlickrApp);
